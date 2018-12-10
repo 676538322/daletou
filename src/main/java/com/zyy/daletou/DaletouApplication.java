5b1e4bf07dbbe9a9d3ed53a7954322543e8bb488
zyy <676538322@qq.com>
2018-12-10 14:01:53 +0800

    first files

---
+++ src/main/java/com/zyy/daletou/DaletouApplication.java
@@ -0,0 +1,12 @@
+package com.zyy.daletou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaletouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaletouApplication.class, args);
+    }
+}
